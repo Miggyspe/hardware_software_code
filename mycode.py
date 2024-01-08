0ae2a73e568b6b0c74ac90d98ff4d183a743db1b
Miggyspe <miggyhenriquez123@gmail.com>
2024-01-08 12:07:13 -0500

    first commit

---
+++ mycode.py
@@ -0,0 +1,11 @@
+def main ():
+    name = "Professor Nedd"
+    print("we want to know if you like programming!")
+    print()
+    print("do you like programming "+ name + "?")
+    answer = input()
+    print("Great! You said " + answer + "?")
+    print("Let's start learning some Pyhton today")
+
+if __name__ == "__main__":
+    main ()
---
+++ mycode2.py
@@ -0,0 +1,13 @@
+
+
+def main ():
+    name = input( "what is your name? ")
+
+    print("we want to know if you like programming!")
+
+    answer=input("Do you like programming " + name ++"? ")
+
+    print( 'Great! you said, '+ '""'+ answer +'""'+ "!")
+    print("Let's start leaning some Python today")
+if __name__== "__main__":
+    main()    
