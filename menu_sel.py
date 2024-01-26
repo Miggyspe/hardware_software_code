72e8de051c262e838b6c3f3d1e643fab151cd682
Miggyspe <miggyhenriquez123@gmail.com>
2024-01-26 08:55:46 -0500

    20240123-hds class

---
+++ menu_sel.py
@@ -0,0 +1,40 @@
+def get_menu(): # used to display a menu
+    menu_dict = {
+        '1':'Apples',
+        '2':'Bananas',
+        '3':'Cherries',
+        '4':'Pears',
+        'X':'Done ordering',
+    }
+    return menu_dict
+
+
+def display_menu(menu_dict):
+    for items, values in menudict.items():
+        print(items+"."+ values.replace('',' ').capitalize())
+    menu_selection = input("What would you like to buy? ").upper()
+
+    print("You selected {}".format(menu_dict[menu_selection]))
+
+    return menu_selection
+
+def display_purchase(items_list):
+    del items_list[-1]
+    print("You purchase {} items".format(len(items_list)), end=" ")
+    print(*items_list, sep=", ", emd=".\n")
+
+def main():
+    menu_selection = ''
+    items_list = []
+    while menu_selection != 'X':
+        menu_dict = get_menu()
+        menu_selection = display_menu(menu_dict)
+        items_list.append(menu_dict[menu_selection])
+        input("Hit Enter to continue!!")
+
+    display_purchase(items_list)
+
+
+
+if name == "main":
+    main()
---
+++ my_loopsA.py
@@ -0,0 +1,17 @@
+def while_counter(num):
+    while num > 0:
+    print("while count:{}".format(num))
+    num += -1
+
+def for_counter(num):
+    while num < 11
+        print("For count:{}".format(count))
+        num += 1
+
+def main():
+    counter = 0
+    while_counter(counter)
+    for_counter(counter)
+
+if __name__ == "__main__":
+    main()
