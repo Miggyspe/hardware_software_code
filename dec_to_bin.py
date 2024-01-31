c41cc80a71e75ac44034a73870137cfd2318c4d8
Miggyspe <miggyhenriquez123@gmail.com>
2024-01-31 11:27:03 -0500

    20240123-hds class

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,16 +1,15 @@
 def decimal_to_binary(number):
     result = ""
     number = int(number)
-    remainder = str(remainder)
-    while number > 0:               #keep dividing unil at 0
+    while number > 0:                     #keep dividing unil at 0
         remainder = number % 2
         number = number // 2
-        result = remainder + result #place string in reverse order
+        result = str(remainder) + result #place string in reverse order
     return result
 
 def main():
     num = input("Enter Binary Number:")
-    print("Decimal {} to Binary: {}".format(num, decimal_to_binary))
+    print("Decimal {} to Binary: {}".format(num, decimal_to_binary(num)))
 
 if __name__ == "__main__":
     main()
